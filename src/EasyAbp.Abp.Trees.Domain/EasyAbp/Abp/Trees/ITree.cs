c9dd20e567889c62607c02620f8b161f5ade410e
YinChang <yincahng0626@gmail.com>
2020-06-19 09:55:44 +0800

    remove TreeRepository.Move method ,and TreeRepository.Update can handle it

--- src/EasyAbp.Abp.Trees.Domain/EasyAbp/Abp/Trees/ITree.cs
+++ src/EasyAbp.Abp.Trees.Domain/EasyAbp/Abp/Trees/ITree.cs
@@ -1,26 +1,42 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Linq;
 using System.Text;
+using System.Xml.XPath;
 using Volo.Abp.Domain.Entities;
 
 namespace EasyAbp.Abp.Trees
 {
-    public interface ITree<TEntity>
-        where TEntity : class, IEntity<Guid>
+    public interface ITree<TEntity>: IEntity<Guid>
+        where TEntity : class, ITree<TEntity>
     {
         string Code { get; set; }
         int Level { get; set; }
         Guid? ParentId { get; set; }
-        IList<TEntity> Children { get; set; }
+        TEntity Parent { get; set; }
+        ICollection<TEntity> Children { get; set; }
         string DisplayName { get; set; }
     }
     public static class ITreeExtensions
     {
-        public static void SetCode<TEntity>(this ITree<TEntity> entity,string code)
-            where TEntity : class, IEntity<Guid>
+        public static void SetCode<TEntity>(this ITree<TEntity> entity, string code)
+            where TEntity : class, ITree<TEntity>
         {
             entity.Code = code;
             entity.Level = entity.Code.Split('.').Length;
+        }
+        public static void MoveTo<TEntity>(this ITree<TEntity> entity, TEntity parent)
+            where TEntity : class, ITree<TEntity>
+        {
+            parent.Children.Add(entity as TEntity);
+            if (entity.Parent != null)
+            {
+                entity.Parent.Children.Remove(entity as TEntity);
+            }
+
+            entity.Parent = parent;
+            entity.ParentId = parent.Id;
+
         }
     }
 }
--- src/EasyAbp.Abp.Trees.Domain/EasyAbp/Abp/Trees/ITreeRepository.cs
+++ src/EasyAbp.Abp.Trees.Domain/EasyAbp/Abp/Trees/ITreeRepository.cs
@@ -12,6 +12,5 @@ namespace EasyAbp.Abp.Trees
         where TEntity : class, IEntity<Guid>, ITree<TEntity>
     {
         Task<List<TEntity>> FindChildrenAsync(Guid? parentId, bool recursive = false);
-        Task MoveAsync(TEntity entity, Guid? parentId, bool autoSave = false, CancellationToken cancellationToken = default);
     }
 }
