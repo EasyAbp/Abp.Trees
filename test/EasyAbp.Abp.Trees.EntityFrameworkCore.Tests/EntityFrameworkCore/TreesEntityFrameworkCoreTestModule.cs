12d5ef35aa91ac94c5f0d302d83af734160a361f
gdlcf88 <gdlcf88@outlook.com>
2024-07-30 16:53:54 +0800

    Update target frameworks

--- test/EasyAbp.Abp.Trees.EntityFrameworkCore.Tests/EntityFrameworkCore/TreesEntityFrameworkCoreTestModule.cs
+++ test/EasyAbp.Abp.Trees.EntityFrameworkCore.Tests/EntityFrameworkCore/TreesEntityFrameworkCoreTestModule.cs
@@ -42,7 +42,7 @@ namespace EasyAbp.Abp.Trees.EntityFrameworkCore
         
         private static SqliteConnection CreateDatabaseAndGetConnection()
         {
-            var connection = new SqliteConnection("Data Source=:memory:");
+            var connection = new AbpUnitTestSqliteConnection("Data Source=:memory:");
             connection.Open();
 
             new TestDbContext(
